91289b313cc72aa047237486d252cd69c3cb729d
jasonisgod <jsonisgod@gmail.com>
2022-07-10 15:15:22 +0800

    b81

--- backend/crawl.py
+++ backend/crawl.py
@@ -2,7 +2,7 @@ from common import *
 
 signal.signal(signal.SIGINT, lambda a,b: exit())
 
-contest_list = ['weekly-contest-301']
+contest_list = ['biweekly-contest-82','biweekly-contest-81','biweekly-contest-80']
 username_list = json.load(open(DIR_CONFIG + '/username.json', 'r'))
 print(contest_list)
 print(username_list)
