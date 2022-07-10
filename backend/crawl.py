9318c6f802cfdd9256b1cf4b99db0f737dd13a12
jasonisgod <jsonisgod@gmail.com>
2022-07-10 15:13:13 +0800

    thread auto query, contest.json updated

--- backend/crawl.py
+++ backend/crawl.py
@@ -3,8 +3,5 @@ from common import *
 signal.signal(signal.SIGINT, lambda a,b: exit())
 
 contest_list = ['weekly-contest-301']
-username_list = json.load(open(DIR_CONFIG + '/username.json', 'r'))
-print(contest_list)
-print(username_list)
+username_list = get_all_users()
 query_contests(contest_list, username_list)
-print(f'\r{SPACES}')
